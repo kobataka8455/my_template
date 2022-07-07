f87ca5d78f665abdc133f1941d2cfdbdeceaaa3a
kobataka8455 <kobataka0505@gmail.com>
2022-07-07 19:12:30 +0900

    add sp

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,17 +5,44 @@ import "@/styles/styles.scss";
 import Swiper, { Autoplay } from 'swiper';
 Swiper.use([Autoplay]);
 
-window.addEventListener("DOMContentLoaded", () => {
+const swiperSet = () => {
   /* eslint-disable no-unused-vars */
   const swiper = new Swiper('.swiper', {
     loop: true,
-    slidesPerView: 5,
     speed: 8000,
+    slidesPerView: 3,
+    spaceBetween: 12,
     autoHeight: true,
-    spaceBetween: 64,
     autoplay: {
       delay: 0,
       disableOnInteraction: false,
     },
+    breakpoints: {
+      519: {
+        slidesPerView: 5,
+        spaceBetween: 64,
+      }
+    }
   });
+};
+
+const spNavSet = () => {
+  const button = document.querySelector('.js-sp-nav-button');
+  const target = document.querySelector('.js-sp-nav');
+  button.addEventListener('click', function () {
+    const isActive = this.classList.contains('is-active');
+    target.style.setProperty("--js-h", target.scrollHeight + "px");
+    if (!isActive) {
+      this.classList.add('is-active');
+      target.classList.add('is-active');
+    } else {
+      this.classList.remove('is-active');
+      target.classList.remove('is-active');
+    }
+  });
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  swiperSet();
+  spNavSet();
 });
